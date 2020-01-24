87fe20068fffe468f80cd87477542135f3eb2f88
Liempo <gonzalesalec@gmail.com>
2020-01-25 02:11:02 +0800

    Planted a tree

--- app/src/main/java/com/liempo/drowsy/MainActivity.kt
+++ app/src/main/java/com/liempo/drowsy/MainActivity.kt
@@ -2,10 +2,13 @@ package com.liempo.drowsy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Timber.plant(Timber.DebugTree())
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
