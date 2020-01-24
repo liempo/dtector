0a8cbff8f19bb2dcb285f8c29d723d35915acc86
Liempo <gonzalesalec@gmail.com>
2020-01-25 05:55:31 +0800

    Fixed photo fragment

--- app/src/main/java/com/liempo/drowsy/gallery/PhotoFragment.kt
+++ app/src/main/java/com/liempo/drowsy/gallery/PhotoFragment.kt
@@ -1,17 +1,27 @@
 package com.liempo.drowsy.gallery
 
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView.ORIENTATION_USE_EXIF
 import com.github.piasy.biv.BigImageViewer
+import com.github.piasy.biv.loader.ImageLoader
 import com.github.piasy.biv.loader.glide.GlideImageLoader
 
 import com.liempo.drowsy.R
+import kotlinx.android.synthetic.main.fragment_photo.*
+import timber.log.Timber
+import java.io.File
+import java.lang.Exception
 
 class PhotoFragment : Fragment() {
+
+    private val args: PhotoFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,5 +37,29 @@ class PhotoFragment : Fragment() {
     ): View? = inflater.inflate(
         R.layout.fragment_photo,
         container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Timber.i("ImagePath: ${args.path}")
+        media_image.showImage(
+            Uri.fromFile(File(args.path)))
+
+        // Fix auto rotate bug
+        // https://github.com/Piasy/BigImageViewer/issues/154
+        media_image.setImageLoaderCallback(object : ImageLoader.Callback {
+
+            override fun onCacheHit(imageType: Int, image: File?) {
+                media_image.ssiv.orientation = ORIENTATION_USE_EXIF
+            }
+
+            override fun onFinish() {}
+            override fun onSuccess(image: File?) {}
+            override fun onFail(error: Exception?) {}
+            override fun onCacheMiss(imageType: Int, image: File?) {}
+            override fun onProgress(progress: Int) {}
+            override fun onStart() {}
+        })
+    }
 
 }
